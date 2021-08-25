fcfee4ade4b2f85954a83981b82765419dea172d
Gilad Lekner <gilad215@gmail.com>
2021-08-25 12:52:12 +0300

    add first imports from shared and internal

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -1,11 +1,9 @@
 import * as React from 'react';
-
-// TODO try to use code from @console/internal
+import { SectionHeading } from '@console/internal/components/utils';
+import { assureEndsWith } from '@console/shared/src/utils';
 
 const Foo: React.FC = () => {
-  return (
-    <div>Hello, KubeVirt!</div>
-  );
+  return <SectionHeading text={assureEndsWith('Hello', 'Kubevirt')} />;
 };
 
 export default Foo;
