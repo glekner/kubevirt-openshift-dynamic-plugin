a079f0107be99cc716ea7f666cb275f3be7aa1a2
Vojtech Szocs <vszocs@redhat.com>
2021-08-25 19:47:57 +0200

    Fix issues

--- src/components/Foo.tsx
+++ src/components/Foo.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
-import { SectionHeading } from '@console/internal/components/utils';
 import { assureEndsWith } from '@console/shared/src/utils';
 
 const Foo: React.FC = () => {
-  return <SectionHeading text={assureEndsWith('Hello', 'Kubevirt')} />;
+  return <div>{assureEndsWith('Hello', 'KubeVirt')}</div>;
 };
 
 export default Foo;
