3a62df63dc95dcee2af134fafe4d70f8c3689e1d
Gilad Lekner <glekner@Gilads-MacBook-Pro.local>
2021-09-01 16:33:24 +0200

    move kubevirt sources to plugin

--- src/components/Foo.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { assureEndsWith } from '@console/shared/src/utils/grammar';
-import { ExternalLink } from '@console/internal/components/utils/link';
-
-const Foo: React.FC = () => {
-  return <ExternalLink href="/foo">{assureEndsWith('Hello', 'KubeVirt')}</ExternalLink>;
-};
-
-export default Foo;
